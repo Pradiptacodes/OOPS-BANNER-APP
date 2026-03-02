9e5ccd12894c5d09de6c082cfbf4d8b56a198fd4
PRADIPTA <pradiptad2006@gmail.com>
2026-03-02 23:47:53 +0530

    Implemented UC2- Render OOPS as banner using print statements

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -9,8 +9,15 @@
  */
 public class OOPSBannerApp {
 
-    public static void main(String[] args) {
-        System.out.println("OOPS");
-    }
+        public static void main(String[] args) {
 
+    System.out.println("  *****   " + "  *****   " + "  ******  " + "  *****   ");
+    System.out.println(" *     *  " + " *     *  " + "  *     * " + " *     *  ");
+    System.out.println(" *     *  " + " *     *  " + "  *     * " + " *        ");
+    System.out.println(" *     *  " + " *     *  " + "  ******  " + "  *****   ");
+    System.out.println(" *     *  " + " *     *  " + "  *        " + "       *  ");
+    System.out.println(" *     *  " + " *     *  " + "  *        " + " *     *  ");
+    System.out.println("  *****   " + "  *****   " + "  *        " + "  *****   ");
+
+        }
 }
